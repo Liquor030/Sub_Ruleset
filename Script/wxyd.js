b70d94d0f9fab49b2f8bae3f8dc431493e6344cf
Liquor <mrhuangbh@gmail.com>
2020-02-28 23:21:24 +0800

    Update wxyd.js

--- Script/wxyd.js
+++ Script/wxyd.js
@@ -1,27 +1,11 @@
 var body = $response.body;
-const path1 = "/pay/memberCardSummary";
-const path2 = "/incentive/cancelFirstCharge";
-var path = $request.path;
-function modify_card() {
-  let obj = JSON.parse(body);
-  obj["expired"] = 0;
-  obj["expiredTime"] = 1591804799;
-  obj["remainTime"] = 86313600;
-  body = JSON.stringify(obj);
-}
-
-function modify_balance() {
-  let obj = JSON.parse(body);
+var url = $request.url;
+const path = "/incentive/cancelFirstCharge";
+let obj = JSON.parse(body);
+if (url.indexOf(path) != -1) {
   obj["hasCancelPayIncentive"] = 1;
   obj["sentChapterCount"] = 3;
   obj["chance"] = 1;
-  body = JSON.stringify(obj);
-   }
-   
-if (path.indexOf(path1) != -1){
-  modify_card();
-}
-if (path.indexOf(path2) != -1 ){
-  modify_balance();
-}
-$done(body);
+	body = JSON.stringify(obj);
+ }
+$done({body});
