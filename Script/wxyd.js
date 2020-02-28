7837905f57d4c3958a315f197a68399291f336f2
Liquor <mrhuangbh@gmail.com>
2020-02-28 23:06:42 +0800

    Create wxyd.js

---
+++ Script/wxyd.js
@@ -0,0 +1,28 @@
+var body = $response.body;
+const path1 = "/pay/memberCardSummary";
+const path2 = "/incentive/cancelFirstCharge";
+var path = $request.path;
+function modify_card() {
+  let obj = JSON.parse(body);
+  obj["remainCoupon"] = 99999;
+  obj["expiredTime"] = 1594223999;
+  obj["expired"] = 0;
+  obj["remainTime"] = 1330440;
+  body = JSON.stringify(obj);
+}
+
+function modify_balance() {
+  let obj = JSON.parse(body);
+  obj["hasCancelPayIncentive"] = 1;
+  obj["sentChapterCount"] = 3;
+  obj["chance"] = 1;
+  body = JSON.stringify(obj);
+   }
+   
+if (path.indexOf(path1) != -1){
+  modify_card();
+}
+if (path.indexOf(path2) != -1 ){
+  modify_balance();
+}
+$done(body);
