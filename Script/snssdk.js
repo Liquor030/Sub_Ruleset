30cbb7c30a6ebf1211b32756d655cba9ec1950a1
Liquor <mrhuangbh@gmail.com>
2020-03-03 00:28:00 +0800

    Create snssdk.js

---
+++ Script/snssdk.js
@@ -0,0 +1,7 @@
+var body = $response.body;
+var url = $request.url;
+const path = "/api/ad/splash";
+if (url.indexOf(path) != -1) {
+  body = '{"data":null,"code":20001,"message":"engine or sati no return ad"}';
+ }
+$done({body});
