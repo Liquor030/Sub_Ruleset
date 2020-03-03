edc31f5d8d377d78cf494e35ca90e9fcf0454a9b
Liquor <mrhuangbh@gmail.com>
2020-03-03 19:33:35 +0800

    Update Super.js

--- Script/Super.js
+++ Script/Super.js
@@ -5,8 +5,7 @@ http-response ^https?://[a-z]*\.snssdk\.com/bds/feed/stream/ requires-body=1,max
 [MITM]
 hostname = *.snssdk.com
 */
-var body = $response.body;
-var obj = JSON.parse(body);
+var obj = JSON.parse($response.body);
 if (obj.data.data) {
   for (var i = obj.data.data.length - 1; i >= 0; i--) {
     if (obj.data.data[i].ad_info != null) {
