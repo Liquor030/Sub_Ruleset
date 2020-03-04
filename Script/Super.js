172fea2e03b40622366f2fb9d1286b49364a387c
Liquor <mrhuangbh@gmail.com>
2020-03-04 15:16:14 +0800

    Update Super.js

--- Script/Super.js
+++ Script/Super.js
@@ -1,2 +1,9 @@
+/* by: Liquor030
+[Script]
+http-response ^https?://.*\.snssdk\.com/bds/feed/stream/ requires-body=1,max-size=-1,script-path=https://raw.githubusercontent.com/Liquor030/Sub_Ruleset/master/Script/Super.js
+
+[MITM]
+hostname = *.snssdk.com
+*/
 var body = $response.body.replace(/{\"cell_type\":2.*?live_info\":null},?/g,'');
 $done({body});
