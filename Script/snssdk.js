6a428f5b4362ef4362681cfa1c27946e082c09b6
Liquor <mrhuangbh@gmail.com>
2020-03-04 15:09:12 +0800

    Update snssdk.js

--- Script/snssdk.js
+++ Script/snssdk.js
@@ -1,6 +1,6 @@
 /*
 [Script]
-http-response ^https?:\/\/.+\.snssdk\.com\/api\/ad\/ script-path= https://raw.githubusercontent.com/Liquor030/Sub_Ruleset/master/Script/snssdk.js,requires-body=true
+http-response ^https?:\/\/.+\.(snssdk|amemv)\.com\/api\/ad\/ script-path= https://raw.githubusercontent.com/Liquor030/Sub_Ruleset/master/Script/snssdk.js,requires-body=true
 
 [MITM]
 hostname = *.snssdk.com
