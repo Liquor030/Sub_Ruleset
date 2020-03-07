cdc6cc9ec6d63231e254d2991291041607255e42
Liquor <mrhuangbh@gmail.com>
2020-03-07 20:52:23 +0800

    Create test.js

---
+++ Script/test.js
@@ -0,0 +1,12 @@
+let body= $response.body;
+var obj = JSON.parse(body);
+if (body.indexOf("expires") !=-1) {
+  obj["receipt"]["receipt_creation_date"] = "2020-03-07 12:50:18 Etc/GMT";
+  obj["receipt"]["in_app"][0]["expires_date"] = "2099-10-19 05:14:18 Etc/GMT";
+  obj["receipt"]["in_app"][0]["expires_date_pst"] = "2099-10-18 22:14:18 America/Los_Angeles";
+  obj["receipt"]["in_app"][0]["expires_date_ms"] = "4096019658000";
+  obj["latest_receipt_info"][0]["expires_date"] = "2099-10-19 05:14:18 Etc/GMT";
+  obj["latest_receipt_info"][0]["expires_date_pst"] = "2099-10-18 22:14:18 America/Los_Angeles";
+  obj["latest_receipt_info"][0]["expires_date_ms"] = "4096019658000";
+  }
+$done({body: JSON.stringify(obj)});
