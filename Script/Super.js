050de7d5b81e1271414ac91ee3a88983d80b5ef6
Liquor <mrhuangbh@gmail.com>
2020-03-10 17:48:38 +0800

    Update Super.js

--- Script/Super.js
+++ Script/Super.js
@@ -5,5 +5,5 @@ http-response ^https?:\/\/.*\.snssdk\.com/bds/feed/stream/ requires-body=1,max-s
 [MITM]
 hostname = *.snssdk.com
 */
-var body = $response.body.replace(/{\"cell_type\":2.*?live_info\":null},?/g,'');
+var body = $response.body.replace(/{\"cell_type\":2.*?_info\":null},?/g,'');
 $done({body});
