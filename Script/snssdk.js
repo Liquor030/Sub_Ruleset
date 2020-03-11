c19ed6a11a6d554817db6df2320ed30616e21d86
Liquor <mrhuangbh@gmail.com>
2020-03-11 09:11:38 +0800

    Update snssdk.js

--- Script/snssdk.js
+++ Script/snssdk.js
@@ -1,9 +1,14 @@
 /* 去除头条系(/api/ad)  by: Liquor030
 [Script]
-http-response ^https?:\/\/.+\.(snssdk|amemv)\.com\/api\/ad\/ script-path= https://raw.githubusercontent.com/Liquor030/Sub_Ruleset/master/Script/snssdk.js,requires-body=true
+http-response ^https?:\/\/.+\.(snssdk|amemv)\.com\/api\/ad\/ script-path= https://raw.githubusercontent.com/Liquor030/Sub_Ruleset/master/Script/snssdk.js
 
 [MITM]
 hostname = *.snssdk.com
 */
-var body = '{"data":null,"code":0,"message":"success"}';
-$done({body});
+var data = {
+  body: "{"data":null,"code":0,"message":"success"}",
+  headers: {
+    "Content-Type": "application/json"
+  }
+};
+$done({response: data});
