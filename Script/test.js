60c80f49b48dcff99a1bdaffb88e6da2db3836ba
Liquor <mrhuangbh@gmail.com>
2020-03-17 21:49:40 +0800

    Update test.js

--- Script/test.js
+++ Script/test.js
@@ -1,21 +1,3 @@
-/*修复版  备用
-[Script]
-http-response ^https?://.*\.snssdk\.com/bds/feed/stream/ requires-body=1,max-size=-1,script-path=https://raw.githubusercontent.com/Liquor030/Sub_Ruleset/master/Script/Super.js
-[MITM]
-hostname = *.snssdk.com
-*/
-var obj = JSON.parse($response.body);
-if (obj.data.data) {
-  for (var i = obj.data.data.length - 1; i >= 0; i--) {
-    if (obj.data.data[i].item.video != null) {
-      obj.data.data[i].item.video.video_download = obj.data.data[i].item.video.video_high
-    }
-    if (obj.data.data[i].ad_info != null) {
-      obj.data.data.splice(i, 1);
-    }
-  }
-}
-var obj2 = JSON.stringify(obj);
-var obj3 = obj2.replace(/\"cell_id\":\d+,\"cell_id_str\":\"(\d+)\"/g,'\"cell_id\":$1,\"cell_id_str\":\"$1\"');
-var body = obj3.replace(/\"item_id\":\d+,\"item_id_str\":\"(\d+)\"/g,'\"item_id\":$1,\"item_id_str\":\"$1\"');
+var obj = $response.body.replace(/{\"cell_type\":2.*?_info\":null},?/g,'');
+var body = obj.replace(/(\"video_download\":.*?\"url_list\":\[)(.*?)(\].*?\"video_high\":.*?\"url_list\":\[)(.*?)(\])/g,'$1$4$3$4$5');
 $done({body});
