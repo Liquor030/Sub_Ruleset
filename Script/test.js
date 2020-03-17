aba662a1ee574991eda241d5a4de9b8060560b7b
Liquor <mrhuangbh@gmail.com>
2020-03-17 22:15:55 +0800

    Update test.js

--- Script/test.js
+++ Script/test.js
@@ -4,20 +4,9 @@ http-response ^https?://.*\.snssdk\.com/bds/feed/stream/ requires-body=1,max-siz
 [MITM]
 hostname = *.snssdk.com
 */
-var obj = JSON.parse($response.body);
-if (obj.data.data) {
-  for (var i = obj.data.data.length - 1; i >= 0; i--) {
-    if (obj.data.data[i].item.video != null) {
-      obj.data.data[i].item.video.video_download.url_list = obj.data.data[i].item.video.video_high.url_list
-    }
-    if (obj.data.data[i].ad_info != null) {
-      obj.data.data.splice(i, 1);
-    }
-  }
-}
-obj = JSON.stringify(obj);
+var obj = $response.body.replace(/{\"cell_type\":2.*?_info\":null},?/g,'');
+obj = obj.replace(/(\"video_download\":.*?\"url_list\":\[)(.*?)(\].*?\"video_high\":.*?\"url_list\":\[)(.*?)(\])/g,'$1$4$3$4$5');
 obj = obj.replace(/\"cell_id\":\d+,\"cell_id_str\":\"(\d+)\"/g,'\"cell_id\":$1,\"cell_id_str\":\"$1\"');
 obj = obj.replace(/tplv-ppx-logo.image/g,'0x0.gif');
-obj = obj.replace(/tplv-ppx-logo/g,'0x0');
-var body = obj.replace(/\"item_id\":\d+,\"item_id_str\":\"(\d+)\"/g,'\"item_id\":$1,\"item_id_str\":\"$1\"');
+var body = obj.replace(/tplv-ppx-logo/g,'0x0');
 $done({body});
