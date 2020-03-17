52608fa9f4294b22c8250e0804f7389d5117d2e2
Liquor <mrhuangbh@gmail.com>
2020-03-17 23:18:37 +0800

    Update test.js

--- Script/test.js
+++ Script/test.js
@@ -10,3 +10,10 @@ obj = obj.replace(/\"video_download\":\{(.*?)\},\"video_low\":null,\"video_mid\"
 obj = obj.replace(/tplv-ppx-logo.image/g,'0x0.gif');
 var body = obj.replace(/tplv-ppx-logo/g,'0x0');
 $done({body});
+
+
+obj = obj.replace(/\"can_download\":(false|null)/g,'\"can_download\":true');
+obj = obj.replace(/\"video_download\":\{(.*?)\},\"video_low\":null,\"video_mid\":null,\"video_high\":\{(.*?)\}/g,'"video_download":{$2},"video_low":null,"video_mid":null,"video_high":{$2}');
+obj = obj.replace(/tplv-ppx-logo.image/g,'0x0.gif');
+var body = obj.replace(/tplv-ppx-logo/g,'0x0');
+$done({body});
