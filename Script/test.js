60743e77902574af5bc97bbd52f6a4c202e257bd
Liquor <mrhuangbh@gmail.com>
2020-03-18 00:17:09 +0800

    Update test.js

--- Script/test.js
+++ Script/test.js
@@ -6,6 +6,7 @@ hostname = *.snssdk.com
 */
 var obj = $response.body.replace(/{\"cell_type\":2.*?_info\":null},?/g,'');
 obj = obj.replace(/\"can_download\":(false|null)/g,'\"can_download\":true');
+obj = obj.replace(/(\"video_download\":.*?\"url_list\":\[)(.*?)(\].*?\"video_high\":.*?\"url_list\":\[)(.*?)(\])/g,'$1$4$3$4$5');
 obj = obj.replace(/\"video_download\":\{(.*?)\},\"video_low\":null,\"video_mid\":null,\"video_high\":\{(.*?)\}/g,'"video_download":{$2},"video_low":null,"video_mid":null,"video_high":{$2}');
 obj = obj.replace(/tplv-ppx-logo.image/g,'0x0.gif');
 var body = obj.replace(/tplv-ppx-logo/g,'0x0');
