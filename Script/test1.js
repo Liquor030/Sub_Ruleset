733a6b3b1acde7d72402e6309c55149066759dda
Liquor <mrhuangbh@gmail.com>
2020-03-18 16:52:27 +0800

    Update test1.js

--- Script/test1.js
+++ Script/test1.js
@@ -8,33 +8,33 @@ var obj = $response.body.replace(/:(\d{19})/g, ':\"$1str\"');
 obj = JSON.parse(obj);
 var c;
 if (obj.data.data) {
-    c = 'data';
+    c = obj.data.data;
 } else if (obj.data.replies) {
-    c = 'replies';
+    c = obj.data.replies;
 } else if (obj.data.cell_comments) {
-    c = 'cell_comments';
+    c = obj.data.cell_comments;
 } else {
     c = null;
 }
 
 if (c != null) {
-    for (var i = obj.data.c.length - 1; i >= 0; i--) {
-        if (obj.data.c[i].ad_info != null) {
-            obj.data.c.splice(i, 1);
+    for (var i = c.length - 1; i >= 0; i--) {
+        if (c[i].ad_info != null) {
+            c.splice(i, 1);
         }
-        if (obj.data.c[i].item != null) {
-            if (obj.data.c[i].item.video != null) {
-                obj.data.c[i].item.video.video_download.url_list = obj.data.c[i].item.origin_video_download.url_list;
+        if (c[i].item != null) {
+            if (c[i].item.video != null) {
+                c[i].item.video.video_download.url_list = c[i].item.origin_video_download.url_list;
             }
-            for (var j = obj.data.c[i].item.comments.length - 1; j >= 0; j--) {
-                if (obj.data.c[i].item.comments[j].video != null) {
-                    obj.data.c[i].item.comments[j].video_download.url_list = obj.data.c[i].item.comments[j].video.url_list;
+            for (var j = c[i].item.comments.length - 1; j >= 0; j--) {
+                if (c[i].item.comments[j].video != null) {
+                    c[i].item.comments[j].video_download.url_list = c[i].item.comments[j].video.url_list;
                 }
             }
         }
-        if (obj.data.c[i].comment_info != null) {
-            if (obj.data.c[i].comment_info.video != null) {
-                obj.data.c[i].comment_info.video_download.url_list = obj.data.c[i].comment_info.video.url_list;
+        if (c[i].comment_info != null) {
+            if (c[i].comment_info.video != null) {
+                c[i].comment_info.video_download.url_list = c[i].comment_info.video.url_list;
             }
         }
     }
