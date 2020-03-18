b12d26f02e1c717c97695ce8a427efbfaab25c1f
Liquor <mrhuangbh@gmail.com>
2020-03-18 19:52:16 +0800

    Delete snssdk.js

--- Script/snssdk.js
+++
@@ -1,8 +0,0 @@
-/* 去除头条系(/api/ad)  by: Liquor030
-[Script]
-http-response ^https?:\/\/.+\.(snssdk|amemv)\.com\/api\/ad\/ script-path= https://raw.githubusercontent.com/Liquor030/Sub_Ruleset/master/Script/snssdk.js
-
-[MITM]
-hostname = *.snssdk.com
-*/
-$done();
