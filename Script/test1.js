5143babb245d3eff100d17e3c743a5cfb72f67d3
Liquor <mrhuangbh@gmail.com>
2020-03-18 18:32:54 +0800

    Update test1.js

--- Script/test1.js
+++ Script/test1.js
@@ -1,4 +1,4 @@
-/*by Liquor remove Super's ad/logo
+/*by Liquor remove Super's ad & logo
 [Script]
 http-response ^https?://.*\.snssdk\.com/bds/feed/stream/ requires-body=1,max-size=-1,script-path=https://raw.githubusercontent.com/Liquor030/Sub_Ruleset/master/Script/Super.js
 [MITM]
@@ -6,7 +6,6 @@ hostname = *.snssdk.com
 */
 var obj = $response.body.replace(/:(\d{19})/g, ':\"$1str\"');
 obj = JSON.parse(obj);
-
 var c;
 if (obj.data.data) {
     c = obj.data.data;
@@ -43,16 +42,24 @@ if (c instanceof Array) {
     }
 } else {
     if (c.comment_info != null) {
+        if (c.item != null) {
+            if (c.item.video != null) {
+                c.item.video.video_download.url_list = c.item.origin_video_download.url_list;
+            }
+            for (var j in c.item.comments) {
+                if (c.item.comments[j].video != null) {
+                    c.item.comments[j].video_download.url_list = c.item.comments[j].video.url_list;
+                }
+            }
+        }
         if (c.comment_info.video != null) {
             c.comment_info.video_download.url_list = c.comment_info.video.url_list;
         }
     }
 }
-
 obj = JSON.stringify(obj);
 obj = obj.replace(/:\"(\d{19})str\"/g, ':$1');
 obj = obj.replace(/\"can_download\":false/g, '\"can_download\":true');
 obj = obj.replace(/tplv-ppx-logo.image/g, '0x0.gif');
 var body = obj.replace(/tplv-ppx-logo/g, '0x0');
-
 $done({body});
