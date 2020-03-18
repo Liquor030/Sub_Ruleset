184664132e5ec66da2a6f5e3ade7060af5331cdb
Liquor <mrhuangbh@gmail.com>
2020-03-18 12:02:27 +0800

    Update test.js

--- Script/test.js
+++ Script/test.js
@@ -1,14 +1,3 @@
-/*去水印测试
-[Script]
-http-response ^https?://.*\.snssdk\.com/bds/feed/stream/ requires-body=1,max-size=-1,script-path=https://raw.githubusercontent.com/Liquor030/Sub_Ruleset/master/Script/Super.js
-[MITM]
-hostname = *.snssdk.com
-*/
-var obj = $response.body.replace(/{\"cell_type\":2.*?_info\":null},?/g,'');
-obj = obj.replace(/\"can_download\":(false|null)/g,'\"can_download\":true');
-obj = obj.replace(/\"video_download\":(.*?)\"url_list\":\[(.*?)\](.*?)\"(video_high|video_fallback)\":(.*?)\"url_list\":\[(.*?)\]/g,'"video_download":$1"url_list":[$6]$3"$4":$5"url_list":[$6]');
-
-
 /*修复版  备用
 [Script]
 http-response ^https?://.*\.snssdk\.com/bds/feed/stream/ requires-body=1,max-size=-1,script-path=https://raw.githubusercontent.com/Liquor030/Sub_Ruleset/master/Script/Super.js
@@ -27,6 +16,7 @@ if (obj.data.data) {
 }
 obj = JSON.stringify(obj);
 obj = obj.replace(/:\"(\d{19})str\"/g,':$1');
+obj = obj.replace(/\"can_download\":(false|null)/g,'\"can_download\":true');
 obj = obj.replace(/tplv-ppx-logo.image/g,'0x0.gif');
 var body = obj.replace(/tplv-ppx-logo/g,'0x0');
 $done({body});
