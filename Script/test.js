c00d0b8296f19452e5d981114c76a7466719d65e
Liquor <mrhuangbh@gmail.com>
2020-04-08 18:14:17 +0800

    Update test.js

--- Script/test.js
+++ Script/test.js
@@ -11,26 +11,8 @@ if (obj.data.data) {
         if (obj.data.data[i].ad_info != null) {
             obj.data.data.splice(i, 1);
         }
-        if (obj.data.data[i].item != null) {
-            if (obj.data.data[i].item.video != null) {
-                obj.data.data[i].item.video.video_download.url_list = obj.data.data[i].item.origin_video_download.url_list;
-            }
-            for (var j = obj.data.data[i].item.comments.length - 1; j >= 0; j--) {
-                if (obj.data.data[i].item.comments[j].video != null) {
-                    obj.data.data[i].item.comments[j].video_download.url_list = obj.data.data[i].item.comments[j].video.url_list;
-                }
-            }
-        }
-        if (obj.data.data[i].comment_info != null) {
-            if (obj.data.data[i].comment_info.video != null) {
-                obj.data.data[i].comment_info.video_download.url_list = obj.data.data[i].comment_info.video.url_list;
-            }
-        }
     }
 }
 obj = JSON.stringify(obj);
 obj = obj.replace(/:\"(\d{19})str\"/g, ':$1');
-obj = obj.replace(/\"can_download\":false/g, '\"can_download\":true');
-obj = obj.replace(/tplv-ppx-logo.image/g, '0x0.gif');
-var body = obj.replace(/tplv-ppx-logo/g, '0x0');
 $done({body});
